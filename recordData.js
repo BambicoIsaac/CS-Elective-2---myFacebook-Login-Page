09e68926adcc69db6fb7a15dc156cee67d23a26b
BambicoIsaac <67004532+BambicoIsaac@users.noreply.github.com>
2022-03-21 23:06:41 +0800

    Added Validation
    Made it so that user wont be able to access the Login Success page if they do not fill in the form fields.

--- recordData.js
+++ recordData.js
@@ -1,8 +1,21 @@
 document.getElementById("myButton").onclick = function () {
+    if(document.getElementById("typeEmailX-2").value=="")
+    {
+        alert("Please Fill in the Email Field");
+        return false;
+    }
+
+    if(document.getElementById("typePasswordX-2").value=="")
+    {
+        alert("Please Fill in the Password Field");
+        return false;
+    }
+
     var inputEmail= document.getElementById("typeEmailX-2");
-    var inputPassword= document.getElementById("typePasswordX-2")
+    var inputPassword= document.getElementById("typePasswordX-2");
     localStorage.setItem("email", inputEmail.value);
     localStorage.setItem("password", inputPassword.value);
     location.href = "loginSuccess.html";
+    return true;
 };
 
